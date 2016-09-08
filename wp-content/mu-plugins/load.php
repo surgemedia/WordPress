7b83b372a342ad671e24f6ea0bcb5022a8dab5f0
Alex <alexanderddking@gmail.com>
2016-09-08 12:39:43 +1000

    added native session

--- wp-content/mu-plugins/load.php
+++ wp-content/mu-plugins/load.php
@@ -1,8 +1,8 @@
 <?php 
 include WPMU_PLUGIN_DIR.'/advanced-custom-fields-pro/acf.php';
 require WPMU_PLUGIN_DIR.'/acf-accordion/acf-accordion.php';
-
-// require WPMU_PLUGIN_DIR.'/ninja-forms/ninja-forms.php';
+// include WPMU_PLUGIN_DIR.'/ninja-forms/ninja-forms.php';
 require WPMU_PLUGIN_DIR.'/acf-ninja-forms/acf-ninja-forms.php';
+require WPMU_PLUGIN_DIR.'/wp-native-php-sessions/pantheon-sessions.php';
 
 ?>

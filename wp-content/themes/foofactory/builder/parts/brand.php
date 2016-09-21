0bb53b839d9102f54a41f559e793b822473ef335
Alex <alexanderddking@gmail.com>
2016-09-21 15:48:36 +1000

    Clean Up merge and remove contaminated styling

--- wp-content/themes/foofactory/builder/parts/brand.php
+++ wp-content/themes/foofactory/builder/parts/brand.php
@@ -1,6 +1,6 @@
 <?php if(strlen($vars['logo']) > 0){ ?>
 		<a class="brand" href="<?= esc_url(home_url('/')); ?>">
-			<img id="logo" src="<?php echo $vars['logo']; ?>" width="420px" height="125px" alt="">
+			<img id="logo" src="<?php echo $vars['logo']; ?>" alt="<?php bloginfo('name'); ?>">
 		</a>
 <?php } else { ?>
 		<a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
